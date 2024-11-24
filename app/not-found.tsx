edb4a355694fa337854287557468184c03f3e65f
Eloho Kennedy <kennyduske@gmail.com>
2024-11-24 14:31:22 +0100

    add custom 404 page

---
+++ app/not-found.tsx
@@ -0,0 +1,9 @@
+export default function Page() {
+  return (
+    <div className="flex justify-center items-center h-svh px-5">
+      <h1 className="font-bold uppercase text-center">
+        We could not find the page you requested for.
+      </h1>
+    </div>
+  );
+}
