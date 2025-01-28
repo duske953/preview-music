fb15f765d4bbcaa5141c52cdfdcc65cf550da7e6
Eloho Kennedy <kennyduske@gmail.com>
2025-01-28 10:37:05 +0100

    add a placeholder image when main image is loading

--- app/components/MusicCard.tsx
+++ app/components/MusicCard.tsx
@@ -1,9 +1,10 @@
 import Image from 'next/image';
 import Link from 'next/link';
-import { Variants, motion } from 'framer-motion';
+import { AnimatePresence, Variants, motion } from 'framer-motion';
 import useStartScrollAnimation from '../hooks/useStartScrollAnimation';
 import useErrorImg from '../hooks/useErrorImg';
 import truncText from '../utils/truncText';
+import { useState } from 'react';
 
 interface MusicCardTypes {
   imgSrc: string;
@@ -15,7 +16,7 @@ interface MusicCardTypes {
   className: string;
 }
 const MotionLink = motion.create(Link);
-
+const MotionImage = motion.create(Image);
 export default function MusicCard({
   imgSrc,
   albumName,
@@ -26,6 +27,7 @@ export default function MusicCard({
   index,
 }: MusicCardTypes) {
   const { imgError, handleErrorImg } = useErrorImg();
+  const [imgLoaded, setImgLoaded] = useState(false);
   const MusicCardVariants: Variants = {
     visible: (index) => ({
       opacity: 1,
@@ -40,6 +42,9 @@ export default function MusicCard({
 
   const { animationRef, controls } = useStartScrollAnimation();
 
+  function handleLoadingComplete() {
+    setImgLoaded(true);
+  }
   return (
     <MotionLink
       ref={animationRef}
@@ -52,14 +57,25 @@ export default function MusicCard({
       href={`/album/${albumId}-${artistId}`}
     >
       <figure className={className}>
-        <Image
-          className="w-full mb-5 rounded-xl"
-          width={5000}
-          onError={handleErrorImg}
-          height={5000}
-          src={imgError.error ? imgError.src : imgSrc}
-          alt={`${albumName} performed by ${artistName}`}
-        />
+        <AnimatePresence>
+          <MotionImage
+            placeholder="blur"
+            blurDataURL="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSI2MDAiIGhlaWdodD0iNDAwIiB2aWV3Qm94PSIwIDAgNjAwIDQwMCI+PHJlY3Qgd2lkdGg9IjEwMCUiIGhlaWdodD0iMTAwJSIgZmlsbD0iIzQ5NTA1NyIvPjwvc3ZnPg=="
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            className={`transition duration-1000 ease-in-out w-full mb-5 rounded-xl ${
+              imgLoaded ? 'opacity-100 blur-none' : 'opacity-0 blur-sm'
+            }`}
+            onError={handleErrorImg}
+            onLoadingComplete={handleLoadingComplete}
+            width={5000}
+            height={5000}
+            src={imgError.error ? imgError.src : imgSrc}
+            alt={`${albumName} performed by ${artistName}`}
+          />
+        </AnimatePresence>
+
         <figcaption className="flex justify-center flex-col items-center">
           <p className="text-xs font-bold">{truncText(albumName, 15)}</p>
           <p className="text-sm text-gray-400">{truncText(artistName, 15)}</p>
